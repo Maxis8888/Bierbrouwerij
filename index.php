adb7b93dd67186f1ebb10154aa96972abb952972
pauljbergmann <pauljbergmann@gmail.com>
2016-04-01 13:40:56 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?= "hello" ?>
 
+<<<<<<< HEAD
+=======
 aherekrke
 
+<<<<<<< Updated upstream
 get Shreked pjaul yee
+=======
+get Shreked pjaul yee
+>>>>>>> origin/master
+>>>>>>> Stashed changes
