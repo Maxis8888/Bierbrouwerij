90adf686fc3caf85f950e0467e27242057c773b5
pauljbergmann <pauljbergmann@gmail.com>
2016-04-05 08:48:55 +0200

    Fuck yeah

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
 <?= "hello" ?>
-
-<<<<<<< HEAD
-=======
-aherekrke
-
-<<<<<<< Updated upstream
-get Shreked pjaul yee
-=======
-get Shreked pjaul yee
->>>>>>> origin/master
->>>>>>> Stashed changes
