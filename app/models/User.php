15e56aa14a1fbf978ac37932136fd5f268c0464e
pauljbergmann <pauljbergmann@gmail.com>
2016-04-08 11:38:02 +0200

    Trying update

--- app/models/User.php
+++ app/models/User.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: paul
- * Date: 05-04-16
- * Time: 11:25
- */
 
 namespace Bierbrouwerij\Models;
 
@@ -15,8 +9,7 @@ class User extends Model
 {
     public function __construct()
     {
-        $this->setPrimaryKey()
+        $this->setTable('users');
+        $this->setPrimaryKey('id');
     }
-
-    protected $primaryKey = 'id';
 }
