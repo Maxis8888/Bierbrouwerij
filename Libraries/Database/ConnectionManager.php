f3065c2578ccb3d74f39e9627e8a5daaa2c952a9
stef538 <stef538@live.nl>
2016-04-08 11:28:20 +0200

    Added a connection Manager

--- Libraries/Database/ConnectionManager.php
+++ Libraries/Database/ConnectionManager.php
@@ -6,16 +6,30 @@ namespace Bierbrouwerij\Libraries\Database;
 class ConnectionManager
 {
     private $host;
-
     private $user;
-
     private $password;
-
     private $database;
-
     private $engine;
 
     public function __construct()
     {
+        // TESTDATA = NOT DEFINITVE.
+        $this->host = '10.88.99.11';
+        $this->user = 'stefvqk_158';
+        $this->password = 'test';
+        $this->database = 'stefvqk_bierbrouwerij';
+        $this->engine = 'mysql';
+    }
+
+    public function connection($host, $user, $password, $database)
+    {
+        try
+        {
+            $host = new PDO($database, $user, $password);
+        }
+        catch(PDOException $e)
+        {
+            echo 'There was an error: '.$e->getMessage();
+        }
     }
 }
