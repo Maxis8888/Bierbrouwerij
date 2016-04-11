0b2d4d6d231906b4af0af3eef14258ebc78b19ab
pauljbergmann <pauljbergmann@gmail.com>
2016-04-11 10:54:45 +0200

    ?

---
+++ app/Controllers/HelloController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Bierbrouwerij\Controllers;
+
+use Illuminate\Routing\Controller;
+
+use Bierbrouwerij\Models\User;
+
+
+/**
+ * Class HelloController
+ *
+ * @author Paul Bergmann <107546@student.drenthecollege.nl>
+ * @package Bierbrouwerij\Controllers
+ */
+class HelloController extends Controller
+{
+    /**
+     * This is the test method.
+     */
+    public function hello()
+    {
+        $array = [
+            'col1',
+            'col2',
+            'col3'
+        ];
+        $user = User::find(69);
+        echo $user->username;
+
+        unset($array[0]);
+        $array = array_values($array);
+
+        print_r($array);
+
+        echo $user->create();
+    }
+
+    /**
+     * This is the index method.
+     */
+    public function index()
+    {
+        echo "Dag";
+    }
+}
